998576bc81d1446968fa87db1a0eaa4d6b201ea8
uneven-coder <185782590+uneven-coder@users.noreply.github.com>
2025-10-18 20:28:42 +0000

    initial page

--- src/topPanel.tsx
+++
@@ -1,8 +0,0 @@
-export default function TopPanel() {
-
-    return (
-        <div className="w-full h-40 bg-base-200 flex items-center justify-center">
-            {/* Game controls and display will go here */}
-        </div>
-    )
-}
