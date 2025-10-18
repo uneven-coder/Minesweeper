b830b6504c4ce327a8577a813542b1f391845f34
Uneven-Coder <185782590+uneven-coder@users.noreply.github.com>
2025-10-18 21:24:29 +0100

    optimised build to create a single html file and assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: './',
+  build: {
+    target: 'es2015',
+    cssCodeSplit: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        format: 'iife',
+        name: 'App',
+        manualChunks: undefined,
+        inlineDynamicImports: true
+      }
+    }
+  }
 });
